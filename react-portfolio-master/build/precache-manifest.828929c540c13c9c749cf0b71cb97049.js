1258ef67b1e1af1273e3972ab49dc928964d362e
jatinbhambhani <jatin.er10192@gmail.com>
2019-05-21 23:56:47 +1000

    v2.0

---
+++ react-portfolio-master/build/precache-manifest.828929c540c13c9c749cf0b71cb97049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b800834e84f4db694af079113daece84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421ab4e7473f26e49a3b",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "180e50162b9036120436",
+    "url": "/static/css/main.b7391a51.chunk.css"
+  },
+  {
+    "revision": "421ab4e7473f26e49a3b",
+    "url": "/static/js/2.98213e63.chunk.js"
+  },
+  {
+    "revision": "180e50162b9036120436",
+    "url": "/static/js/main.4b333425.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
